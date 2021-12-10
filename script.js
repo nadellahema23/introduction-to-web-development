6f6ab0889eac5076d7452b0ea962cb3797333027
nadellahema23 <39938063+nadellahema23@users.noreply.github.com>
2021-12-10 20:24:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function compute() {
     var interest = principal * years * rate / 100;
     var yearInTheFuture = new Date().getFullYear() + years;
     //Create the Interest text
-    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+    document.getElementById("result").innerHTML = " If you deposit <mark>" + principal + "</mark>,<br/>" +
         "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
         "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
         "in the year <mark>" + yearInTheFuture + "</mark>";
